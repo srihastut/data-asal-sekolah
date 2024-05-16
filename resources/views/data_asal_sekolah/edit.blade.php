51e7dcdde524350f7299f91b941b6daa563fdb00
srihastut <149176809+srihastut@users.noreply.github.com>
2024-05-16 15:49:20 +0700

    Update edit.blade.php

--- resources/views/data_asal_sekolah/edit.blade.php
+++ resources/views/data_asal_sekolah/edit.blade.php
@@ -1,20 +1,31 @@
 @extends('layout/aplikasi')
 
 @section('konten')
-    <a href="/data_asal_sekolah" class="btn btn-secondary"><< kembali</a>
-    <form method="post" action="{{ url('/data_asal_sekolah/'.$data->kode_asal_sekolah) }}">
+    <form method="post" action="{{ url('/data_asal_sekolah/' . $data->kode_asal_sekolah) }}">
         @csrf
-        @method('PUT')
+        @method('PUT') <!-- Perhatikan ini untuk menggunakan metode PUT dalam formulir edit -->
+
         <div class="mb-3">
-            <h1>Kode Asal Sekolah: {{$data->kode_asal_sekolah}}</h1>
+            <label for="kode_asal_sekolah" class="form-label">Kode Asal Sekolah</label>
+            <input type="text" class="form-control" name="kode_asal_sekolah" id="kode_asal_sekolah" value="{{ $data->kode_asal_sekolah }}">
         </div>
         <div class="mb-3">
             <label for="nama_asal_sekolah" class="form-label">Nama Asal Sekolah</label>
-            <input type="text" class="form-control" name="nama_asal_sekolah" id="nama_asal_sekolah" value="{{$data->nama_asal_sekolah}}">
+            <input type="text" class="form-control" name="nama_asal_sekolah" id="nama_asal_sekolah" value="{{ $data->nama_asal_sekolah }}">
         </div>
+        <div class="form-group">
+            <label for="jenis_asal_sekolah" class="form-label">Jenis Asal Sekolah</label>
+            <select class="form-control select2" style="width: 100%;" name="jenis_asal_sekolah" id="jenis_asal_sekolah">
+                <option disabled>Pilih Jenis asal sekolah</option>
+                @foreach ($jenis as $item)
+                    <!-- Perhatikan penambahan selected jika jenis_asal_sekolah sama dengan jenis yang sedang di-loop -->
+                    <option value="{{ $item->id }}" {{ $item->id == $data->jenis_asal_sekolah ? 'selected' : '' }}>{{ $item->jenis }}</option>
+                @endforeach
+            </select>
+        </div>        
         <div class="mb-3">
             <label for="deskripsi" class="form-label">Deskripsi</label>
-            <input type="text" class="form-control" name="deskripsi" id="deskripsi" value="{{ $data->deskripsi}}">
+            <input type="text" class="form-control" name="deskripsi" id="deskripsi" value="{{ $data->deskripsi }}">
         </div>
         <div class="mb-3">
             <label for="kota_asal_sekolah" class="form-label">Kota Asal Sekolah</label>
@@ -22,10 +33,10 @@
         </div>
         <div class="mb-3">
             <label for="provinsi_asal_sekolah" class="form-label">Provinsi Asal Sekolah</label>
-            <textarea class="form-control" name="provinsi_asal_sekolah">{{$data->provinsi_asal_sekolah }}</textarea>
+            <textarea class="form-control" name="provinsi_asal_sekolah">{{ $data->provinsi_asal_sekolah }}</textarea>
         </div>
         <div class="mb-3">
-            <button type="submit" class="btn btn-primary">UPDATE</button>
+            <button type="submit" class="btn btn-primary">SIMPAN</button>
         </div>
     </form>
 @endsection
