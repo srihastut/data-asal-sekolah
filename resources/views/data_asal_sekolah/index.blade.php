d111880682ba654dd66ccf8aa0fb4762422a8f35
srihastut <149176809+srihastut@users.noreply.github.com>
2024-05-29 11:40:22 +0700

    Update index.blade.php

--- resources/views/data_asal_sekolah/index.blade.php
+++ resources/views/data_asal_sekolah/index.blade.php
@@ -5,11 +5,12 @@
 <div class="pb-3">
     <form class="d-flex" action="{{url('data_asal_sekolah')}}" method="get">
         <input class="form-control me-1" type="search" name="katakunci" value="{{ Request::get('katakunci') }}" placeholder="Masukkan kata kunci" aria-label="Search">
-        <button class="btn btn-secondary" type="submit">Cari</button>
+        <button class="btn btn-secondary" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
     </form>
   </div>
-    <a href="/data_asal_sekolah/create" class="btn btn-primary mb-2">+ Tambah Data Asal Sekolah</a>
-    <a href="{{url('/cetak')}}" target="_blank" class="btn btn-primary mb-2">Cetak Data <i class="fa-light fa-print"></i>
+    <a href="/data_asal_sekolah/create" class="btn btn-primary mb-2"><i class="fa-regular fa-square-plus"></i> Tambah Data</a>
+    <a href="{{url('/cetak')}}" target="_blank" class="btn btn-primary mb-2">Cetak Data  <i class="fa-solid fa-download"></i>
+        
 </a>
     <table class="table">
         <thead>
@@ -34,12 +35,12 @@
                     <td>{{ $item->provinsi_asal_sekolah }}</td>
                     <td>
                         <div class="btn-group" role="group">
-                            <a class='btn btn-secondary btn-sm' href="{{ url('/data_asal_sekolah/'.$item->kode_asal_sekolah) }}">Detail</a>
-                            <a class='btn btn-warning btn-sm' href="{{ url('/data_asal_sekolah/'.$item->kode_asal_sekolah.'/edit') }}">Edit</a>
+                            <a class='btn btn-secondary btn-sm' href="{{ url('/data_asal_sekolah/'.$item->kode_asal_sekolah) }}"><i class="fa-solid fa-circle-info"></i>Detail</a>
+                            <a class='btn btn-warning btn-sm' href="{{ url('/data_asal_sekolah/'.$item->kode_asal_sekolah.'/edit') }}"><i class="fa-solid fa-pen-to-square"></i>Edit</a>
                             <form onsubmit="return confirm('Apakah ingin menghapus data')" class='d-inline' action="{{ '/data_asal_sekolah/'.$item->kode_asal_sekolah }}" method='post'>
                                 @csrf
                                 @method('DELETE')
-                                <button class="btn btn-danger btn-sm ml-1" type="submit">Del</button>
+                                <button class="btn btn-danger btn-sm ml-1" type="submit"><i class="fa-solid fa-trash-can"></i>Delete</button>
                             </form>
                         </div>
                     </td>
